bd6fb7bcd6e031a7a525c463e028593cfbe8e84d
unknown <ranatanveerhassan04@gmail.com>
2023-11-06 21:51:57 +0500

    ok

---
+++ app.js
@@ -0,0 +1,103 @@
+alert("welcome to my website Rana Tanveer Hassan")
+
+// for(var i = -10; i <= 10; i++) {
+//     for (var j=i; j<i+10; j++)
+//     document.write(j);
+// document.write("<br>")
+
+// }
+
+
+// // var i=prompt("enter the number")
+// for (var i = 5; i <=60; i=i+5){
+//         document.write(i)
+//     document.write("<br>")
+// }
+
+for (var i = 9; i >=0;i--){
+    for(var b=1;b<=i;b++){
+   document.write("1");}
+document.write("<br>")
+}
+
+   
+
+document.write("<hr>")
+
+
+
+document.write("<center>")
+for (var i = 9; i >=0;i--){
+    for(var b=1;b<=i;b++){
+   document.write("*");}
+document.write("<br>")
+}
+document.write("</center>")
+
+
+
+document.write("<hr>")
+document.write("<hr>")
+document.write("<hr>")
+document.write("<center>")
+for (var i = 0; i <=9;i++){
+    for(var b=1;b<=i;b++){
+   document.write("*");}
+document.write("<br>")
+}document.write("</center>")
+
+
+
+document.write("<hr>")
+
+
+
+for (var i = 1; i <=9;i++){
+    for(var b=1;b<=i;b++){
+   document.write("1");}
+document.write("<br>")}
+
+
+
+document.write("<hr>")
+
+
+
+document.write("<center>")
+
+for (var i = 1; i <=12; i+=2){
+    for(var b=1;b<=i;b++){
+   document.write("*" + " ");}
+document.write("<br>")
+    }
+    document.write("</center>")
+
+
+
+    document.write("<hr>")
+    document.write("<hr>")
+    document.write("<hr>")
+
+    document.write("<center>")
+
+    for (var i = 12; i >=1; i-=2){
+        for(var b=1;b<=i;b++){
+       document.write("*" + " ");}
+    document.write("<br>")
+        }
+        document.write("</center>")
+
+
+        document.write("<hr>")
+        document.write("<hr>")
+
+
+
+        document.write("<center>")
+
+    for (var i = 1; i <=9; i++){
+        for(var b=1;b<=i; b++){
+       document.write(b + " ");}
+    document.write("<br>")
+        }
+        document.write("</center>")
